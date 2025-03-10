58bab20622959109d215788c7b9fddbb2d50903e
VINIshow1907 <viniciuslucio792@gmail.com>
2025-03-10 14:16:16 -0300

    :sparkles: Configurando atributos para mandar para o banco

---
+++ 1 SEMESTRE/FatecADS/fatecads/src/main/java/br/com/fatecads/fatecads/entity/Aluno.java
@@ -0,0 +1,38 @@
+package br.com.fatecads.fatecads.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class Aluno {
+
+   @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer idAluno;
+ 
+    @Column(nullable = false, length = 40)
+    private String nomeAluno;
+ 
+    @Column(nullable = false, length = 40)
+    private String endAluno;
+ 
+    @Column(nullable = false, length = 11)
+    private String telAluno;
+ 
+    private Integer raAluno;
+
+
+
+}
