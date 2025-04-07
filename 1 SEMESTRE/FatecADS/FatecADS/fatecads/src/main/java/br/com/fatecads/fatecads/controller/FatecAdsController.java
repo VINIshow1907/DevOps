1403b3e92da3d074be2dcec0bbfdf924db5bf4da
VINIshow1907 <viniciuslucio792@gmail.com>
2025-04-07 15:23:06 -0300

    :sparkles: feat: Página de inicial

---
+++ 1 SEMESTRE/FatecADS/FatecADS/fatecads/src/main/java/br/com/fatecads/fatecads/controller/FatecAdsController.java
@@ -0,0 +1,19 @@
+package br.com.fatecads.fatecads.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+@RequestMapping("/fatecads")
+public class FatecAdsController {
+
+    @GetMapping
+    public String index(Model model) {
+        //Retorna a página index.html
+        return "/index";
+    }
+    
+}
