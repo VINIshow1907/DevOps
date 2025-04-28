5f57228dddf0dbd7c8fdf3b273a0679e9193450b
VINIshow1907 <viniciuslucio792@gmail.com>
2025-04-28 13:51:26 -0300

    :tada: init: Subindo projeto

---
+++ 1 SEMESTRE/ExemploTest/exemplotest/src/main/java/br/com/exemplotest/exemplotest/ExemplotestApplication.java
@@ -0,0 +1,13 @@
+package br.com.exemplotest.exemplotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemplotestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemplotestApplication.class, args);
+	}
+
+}
---
+++ 1 SEMESTRE/ExemploTest/exemplotest/src/test/java/br/com/exemplotest/exemplotest/ExemplotestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.exemplotest.exemplotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemplotestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
