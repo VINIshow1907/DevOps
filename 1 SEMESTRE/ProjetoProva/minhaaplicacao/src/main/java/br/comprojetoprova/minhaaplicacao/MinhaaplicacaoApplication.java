9d938ba39c7867ef9bf28cffdcea628e0ce2752e
VINIshow1907 <viniciuslucio792@gmail.com>
2025-06-15 14:34:43 -0300

    :tada: init: Criando projeto

---
+++ 1 SEMESTRE/ProjetoProva/minhaaplicacao/src/main/java/br/comprojetoprova/minhaaplicacao/MinhaaplicacaoApplication.java
@@ -0,0 +1,13 @@
+package br.comprojetoprova.minhaaplicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaaplicacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaaplicacaoApplication.class, args);
+	}
+
+}
---
+++ 1 SEMESTRE/ProjetoProva/minhaaplicacao/src/test/java/br/comprojetoprova/minhaaplicacao/MinhaaplicacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.comprojetoprova.minhaaplicacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaaplicacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
