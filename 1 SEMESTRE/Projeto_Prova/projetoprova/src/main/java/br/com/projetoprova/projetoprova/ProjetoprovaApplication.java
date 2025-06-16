b996f40f5dcb99d4037418b0ec58f6ae982904e4
VINIshow1907 <viniciuslucio792@gmail.com>
2025-06-16 12:52:57 -0300

    :tada: init: Criando Projeto correto

--- 1 SEMESTRE/Projeto_Prova/projetoprova/src/main/java/br/com/projetoprova/projetoprova/ProjetoprovaApplication.java
+++ 1 SEMESTRE/Projeto_Prova/projetoprova/src/main/java/br/com/projetoprova/projetoprova/ProjetoprovaApplication.java
@@ -1,13 +1,13 @@
-package br.comprojetoprova.minhaaplicacao;
+package br.com.projetoprova.projetoprova;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MinhaaplicacaoApplication {
+public class ProjetoprovaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MinhaaplicacaoApplication.class, args);
+		SpringApplication.run(ProjetoprovaApplication.class, args);
 	}
 
 }
--- 1 SEMESTRE/Projeto_Prova/projetoprova/src/test/java/br/com/projetoprova/projetoprova/ProjetoprovaApplicationTests.java
+++ 1 SEMESTRE/Projeto_Prova/projetoprova/src/test/java/br/com/projetoprova/projetoprova/ProjetoprovaApplicationTests.java
@@ -1,10 +1,10 @@
-package br.comprojetoprova.minhaaplicacao;
+package br.com.projetoprova.projetoprova;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MinhaaplicacaoApplicationTests {
+class ProjetoprovaApplicationTests {
 
 	@Test
 	void contextLoads() {
