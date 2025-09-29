57c4c29b3c546152140c35be2740eebed0091c26
VINIshow1907 <viniciuslucio792@gmail.com>
2025-09-29 14:17:26 -0300

    :sparkles: feat: Fazendo relação N para N

--- 1 SEMESTRE/FatecADS/FatecADS/fatecads/src/main/java/br/com/fatecads/fatecads/entity/Curso.java
+++ 1 SEMESTRE/FatecADS/FatecADS/fatecads/src/main/java/br/com/fatecads/fatecads/entity/Curso.java
@@ -1,10 +1,15 @@
 package br.com.fatecads.fatecads.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -33,5 +38,10 @@ public class Curso {
     @Column(nullable = false, length = 11)
     private String periodoCurso;
      
+    @ManyToMany
+    @JoinTable(name = "curso_disciplina",joinColumns = @JoinColumn(name = "curso_id"),inverseJoinColumns = @JoinColumn(name = "disciplina_id")
+    ) 
+
+    private List<Disciplina> disciplinas;
 }
 
---
+++ 1 SEMESTRE/FatecADS/FatecADS/fatecads/src/main/java/br/com/fatecads/fatecads/service/DisciplinaSerice.java
@@ -0,0 +1,31 @@
+package br.com.fatecads.fatecads.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.fatecads.fatecads.entity.Disciplina;
+import br.com.fatecads.fatecads.repository.DisciplinaRepository;
+
+public class DisciplinaSerice {
+
+    @Autowired
+    private DisciplinaRepository disciplinaRepository;
+
+    public Disciplina save(Disciplina disciplina) {
+        return disciplinaRepository.save(disciplina);
+    }
+    public List<Disciplina> findAll() {
+        return disciplinaRepository.findAll();
+    }
+    public void deleteById(Long id) {
+        disciplinaRepository.deleteById(id);
+    }
+    public Disciplina findById(Long id) {
+        return disciplinaRepository.findById(id).orElse(null);
+    }
+    public List<Disciplina> findAllById(List<Long> ids) {
+        return disciplinaRepository.findAllById(ids);
+    }
+    
+}
