912247a4162a90a9e371e7ae2c0cad5c5c724115
VINIshow1907 <viniciuslucio792@gmail.com>
2025-11-10 14:10:38 -0300

    :zap: perf: Criando repository ItemPedido

---
+++ 1 SEMESTRE/LOJADS/lojaads/src/main/java/br/com/lojaads/lojaads/repository/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package br.com.lojaads.lojaads.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.lojaads.lojaads.entity.ItemPedido;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
+
+}
