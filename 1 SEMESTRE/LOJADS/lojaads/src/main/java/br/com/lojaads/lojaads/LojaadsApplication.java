4216e25315131791739198f1f9c378415c06f2dd
VINIshow1907 <viniciuslucio792@gmail.com>
2025-11-10 13:12:40 -0300

    :tada: init: projeto Loja

---
+++ 1 SEMESTRE/LOJADS/lojaads/src/main/java/br/com/lojaads/lojaads/LojaadsApplication.java
@@ -0,0 +1,13 @@
+package br.com.lojaads.lojaads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaadsApplication.class, args);
+	}
+
+}
---
+++ 1 SEMESTRE/LOJADS/lojaads/src/test/java/br/com/lojaads/lojaads/LojaadsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lojaads.lojaads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
