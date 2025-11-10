7f754b5c1e7d831c214e419b41e220a11f9bd927
VINIshow1907 <viniciuslucio792@gmail.com>
2025-11-10 14:10:46 -0300

    :zap: perf: Criando repository Pedido

---
+++ 1 SEMESTRE/LOJADS/lojaads/src/main/java/br/com/lojaads/lojaads/repository/PedidoRepository.java
@@ -0,0 +1,24 @@
+package br.com.lojaads.lojaads.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import br.com.lojaads.lojaads.entity.Pedido;
+import br.com.lojaads.lojaads.entity.Usuario;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Long>{
+
+    List<Pedido> findByUsuario(Usuario usuario);
+
+    @Query("SELECT p FROM Pedido p WHERE p.usuario = :usuario AND SIZE(P.itens) > 0")
+
+    Optional<Pedido> findByUsuarioAndItensIsNoEmpty(@Param("usuario") Usuario usuario);
+
+    Optional<Pedido> findByUsuarioAndFinalizadoFalse(Usuario usuario);
+
+
+}
